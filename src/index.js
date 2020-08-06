b4f7b9ee26fb55ef91922329aaa5d7e79cb22c90
kaytbode <bkaoluborode@gmail.com>
2020-08-06 12:27:37 +0100

    fix: cors issue

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(routes);
+app.use('/', routes);
 
 app.listen(process.env.PORT);
